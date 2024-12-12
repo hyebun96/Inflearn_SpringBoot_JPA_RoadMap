b1a1a7d3c7926b228122c90c877cbd3e71d42744
hyebun96 <hyebun96@naver.com>
2024-12-12 15:39:18 +0900

    refactor: 영속성컨텍스트 - 준영속상태

--- 1. 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ 1. 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -13,15 +13,21 @@ public class JpaMain {
 
         try {
 
-            Member member = new Member(200L, "member200");
-            em.persist(member);
+            // 영속
+            Member member = em.find(Member.class, 150L);
+            member.setName("AAAAA");
 
-            // 플러시
-            em.flush();
+            // detch : 영속성컨텍스트가 더이상 관리하지 않아서 update되지 않음
+            // em.detach(member);
+
+            // clear : 영속컨텍스트 전부 지워짐
+            em.clear();
+
+            // 다시 초기화되어서 DB에서 값 다시 가져옴
+            Member member1 = em.find(Member.class, 150L);
 
             System.out.println("========");
 
-            // flush 되는 순간에 현 Entity와 스냅샷을 비교하여 다르면 update해줌
             tx.commit();
 
         } catch (Exception e) {
