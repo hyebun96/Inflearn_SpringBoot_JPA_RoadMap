bcca91747d6639576ae0dee91c4f173cb8ee3bc0
hyebun96 <hyebun96@naver.com>
2024-12-13 22:14:23 +0900

    feat: 필드와 컬럼매핑(@Column, @Temporal, @Enumerated, @Lob, @Transient)

--- 1. 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ 1. 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,36 +1,39 @@
 package hellojpa;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.Date;
 
 @Entity
 public class Member {
 
     @Id
     private Long id;
-    private String name;
+
+    @Column(name = "name", nullable = false)
+    private String username;
+
+    private Integer age;
+
+    // default ordinal : 숫자로 DB에 저장됨(운영상 위험 : 변경될 경우 값의 무결성을 보호하지 않음)
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    // 지금은 LocalDate, LocalDateTime 권장
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    // 문자면 CLOB, 숫자면 BLOB 으로 매핑
+    @Lob
+    private String description;
+
+    // 매핑 안할경
+   @Transient
+    private int temp;
 
     public Member() {
-    }
-
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 }
