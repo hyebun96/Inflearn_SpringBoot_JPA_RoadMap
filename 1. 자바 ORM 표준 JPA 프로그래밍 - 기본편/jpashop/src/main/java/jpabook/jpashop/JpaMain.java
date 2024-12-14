fb60e389d19fc43a2a6aac941bd5ce5bd91ae760
hyebun96 <hyebun96@naver.com>
2024-12-15 03:50:01 +0900

    feat: 양방향 연관관계 - 연관관계 편의 메서드추가

--- 1. 자바 ORM 표준 JPA 프로그래밍 - 기본편/jpashop/src/main/java/jpabook/jpashop/JpaMain.java
+++ 1. 자바 ORM 표준 JPA 프로그래밍 - 기본편/jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -4,6 +4,8 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
 
 public class JpaMain {
 
@@ -16,6 +18,12 @@ public class JpaMain {
 
         try {
 
+            Order order = new Order();
+            em.persist(order);
+
+            OrderItem orderItem = new OrderItem();
+            orderItem.setOrder(order);
+            em.persist(orderItem);
 
             tx.commit();
 
