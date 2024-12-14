b02309a181b8ec08cdaf4238e61ccb2af6ee444f
hyebun96 <hyebun96@naver.com>
2024-12-15 05:25:28 +0900

    feat: 실전예제 - 요구사항 추가, 다양한 연관관계 매핑

--- 1. 자바 ORM 표준 JPA 프로그래밍 - 기본편/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ 1. 자바 ORM 표준 JPA 프로그래밍 - 기본편/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -18,6 +18,10 @@ public class Order {
     @JoinColumn(name = "MEMBER_ID")
     private Member member;
 
+    @OneToOne
+    @JoinColumn(name = "DELIVERY_ID")
+    private Delivery delivery;
+
     // order에서
     @OneToMany(mappedBy = "order")
     private List<OrderItem> orderItems = new ArrayList<>();
