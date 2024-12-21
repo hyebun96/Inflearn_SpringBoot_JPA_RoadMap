df5d8a1d8092cdbe836b3f9c84dd283ecf750850
hyebun <hyebun96@naver.com>
2024-12-21 17:27:49 +0900

    feat: JPQL 타입표현과 기타식

--- 1. 자바 ORM 표준 JPA 프로그래밍 - 기본편/hellojpql/src/main/java/jpql/JpaMainSubQuery.java
+++ 1. 자바 ORM 표준 JPA 프로그래밍 - 기본편/hellojpql/src/main/java/jpql/JpaMainSubQuery.java
@@ -25,7 +25,7 @@ public class JpaMainSubQuery {
             Member member = new Member();
             member.setUsername("member1");
             member.setAge(1);
-
+            member.setType(MemberType.USER);
             member.setTeam(team);
 
             em.persist(member);
@@ -35,15 +35,32 @@ public class JpaMainSubQuery {
 
             // 서브쿼리의 한계
             // JPA는 WHERE, HAVING 절에서만 서브 쿼리 가능
-            // FROM 절은 불가능
+            // FROM 절은 하이버네이트 6부터 지원
             // SELECT 절은 하이버네이트에서 지원
-            String query = "select (select avg(m1)from Member m1) from Member m join m.team t";
-            List<Member> result = em.createQuery(query, Member.class)
-                    .setFirstResult(0)
-                    .setMaxResults(10)
-                    .getResultList();
+            String query = "select m from Member m where m.team = (select t from Team t)";
+            List resultList = em.createQuery(query)
+                                .getResultList();
 
-            System.out.println("result = " + result.size());
+            System.out.println("result = " + resultList.size());
+
+            em.flush();
+            em.clear();
+
+
+            // JPQL 타입표현
+            // ENUM : jpbook, MemberType.Admin(패키지 명 포함)
+            String query2 = "select m.username, 'HELLO', TRUE from Member m where m.type = :userType";
+            List<Object[]> result2 = em.createQuery(query2)
+                                        .setParameter("userType", jpql.MemberType.USER)
+                                        .getResultList();
+
+            System.out.println("size = " +result2.size());
+
+            for (Object[] objects : result2) {
+                System.out.println("objects = " + objects[0]);
+                System.out.println("objects = " + objects[1]);
+                System.out.println("objects = " + objects[2]);
+            }
 
             tx.commit();
 
