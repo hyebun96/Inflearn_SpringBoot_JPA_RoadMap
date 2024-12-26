790504aa5af8d2d53506716821aa16130ea1c300
hyebun <hyebun96@naver.com>
2024-12-26 15:24:33 +0900

    feat: 회원도메인- 회원서비스 개발

---
+++ 2. 실전! 스프링부트와 JPA 활용1 - 웹 애플리케이션 개발/jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,44 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    // 회원가입
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicateMember(member);    // 중복 회원 검증
+        return memberRepository.save(member);
+    }
+
+    private void validateDuplicateMember(Member member) {
+        // EXCEPTION
+        // 멀티스레드 동시성 문제 해결을 위해서는 member.name을 unique 잡길 권장
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if(!findMembers.isEmpty()) {
+            throw new IllegalArgumentException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    public List<Member> findMember() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId) {
+        return memberRepository.find(memberId);
+    }
+
+
+}
