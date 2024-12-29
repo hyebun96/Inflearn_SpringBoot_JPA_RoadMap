43a2e50a182628d29b928be96ced90f332c29b97
hyebun <hyebun96@naver.com>
2024-12-29 20:45:10 +0900

    feat: 주문도메인- 주문 서비스 개발, OrderItem 생성자 protected 처리(생성메서드 사용 권장)

--- 2. 실전! 스프링부트와 JPA 활용1 - 웹 애플리케이션 개발/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ 2. 실전! 스프링부트와 JPA 활용1 - 웹 애플리케이션 개발/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -2,13 +2,16 @@ package jpabook.jpashop.domain;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import static jakarta.persistence.FetchType.LAZY;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)  // 생성자 protected 사용해 new 하지 못하게 막음 -> 생성메서드 사용하라는 의미
 public class OrderItem {
 
     @Id @GeneratedValue
