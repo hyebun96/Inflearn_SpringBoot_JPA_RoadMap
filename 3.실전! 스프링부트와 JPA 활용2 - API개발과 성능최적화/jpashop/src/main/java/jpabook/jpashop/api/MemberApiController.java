df11a10ac379183d11c7995fc3ef06f77126d43d
hyebun <hyebun96@naver.com>
2025-01-01 01:36:22 +0900

    feat: API 기본 - 회원등록API

---
+++ 3.실전! 스프링부트와 JPA 활용2 - API개발과 성능최적화/jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -0,0 +1,55 @@
+package jpabook.jpashop.api;
+
+import jakarta.validation.Valid;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+
+    private final MemberService memberService;
+
+    /* 등록 v1의 문제점
+        1. 엔티티에 프레젠테이션 계층을 위한 로직이 추가됨
+        2. 실무에서는 회원 엔티티를 다양하게 사용되는데 이 API만을 위한 요구사항을 담는것은 불필요하다.
+        -> API 요청 스펙에 맞추어 별도 DTO를 받는것이 최적화 */
+    @PostMapping("api/v1/members")
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    /* 등록 V2
+        1. 엔티티와 프레젠테이션 계층을 위한 로직 분리가능
+        2. 엔티티와 API 스펙을 명확하게 분리
+        3. 엔티티가 변해도 API 스펙이 변하지 않음*/
+    @PostMapping("/api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
+
+        Member member = new Member();
+        member.setName(request.getName());
+
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @Data
+    static class CreateMemberResponse {
+        private Long id;
+
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
+    }
+
+    @Data
+    static class CreateMemberRequest {
+        private String name;
+    }
+}
