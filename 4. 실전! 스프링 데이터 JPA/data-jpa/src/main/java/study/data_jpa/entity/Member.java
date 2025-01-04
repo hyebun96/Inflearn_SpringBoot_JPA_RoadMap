b986fff98e411c83302811b0d32502caa6f869e8
hyebun <hyebun96@naver.com>
2025-01-04 17:45:01 +0900

    feat: 예제 도메인 모델과 동작확인

--- 4. 실전! 스프링 데이터 JPA/data-jpa/src/main/java/study/data_jpa/entity/Member.java
+++ 4. 실전! 스프링 데이터 JPA/data-jpa/src/main/java/study/data_jpa/entity/Member.java
@@ -1,26 +1,53 @@
 package study.data_jpa.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.NoSuchElementException;
+
+import static jakarta.persistence.FetchType.*;
 
 @Entity
 @Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username,", "age"})
 public class Member {
 
     @Id @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
     private String username;
+    private int age;
 
-    protected Member() {
-    }
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
 
     public Member(String username) {
         this.username = username;
     }
 
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if(team != null){
+            changeTeam(team);
+        } else {
+            new NoSuchElementException("팀이 존재하지 않습니다.");
+        }
+    }
+
     public void changeUsername(String username) {
         this.username = username;
     }
+
+    /* 연관관계 세팅 */
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
+
 }
