97ce6fb65aff7311651bb12c583022c5f1f41d6a
hyebun <hyebun96@naver.com>
2025-01-05 23:59:42 +0900

    feat: 쿼리메소드기능 - 순수JPA 페이징과 정렬

--- 4. 실전! 스프링 데이터 JPA/data-jpa/src/test/java/study/data_jpa/repository/MemberJpaRepositoryTest.java
+++ 4. 실전! 스프링 데이터 JPA/data-jpa/src/test/java/study/data_jpa/repository/MemberJpaRepositoryTest.java
@@ -106,4 +106,31 @@ public class MemberJpaRepositoryTest {
         assertThat(findMember).isEqualTo(m1);
     }
 
+    @Test
+    public void paging() {
+        // Given
+        memberJpaRepository.save(new Member("member1", 10));
+        memberJpaRepository.save(new Member("member2", 10));
+        memberJpaRepository.save(new Member("member3", 10));
+        memberJpaRepository.save(new Member("member4", 10));
+        memberJpaRepository.save(new Member("member5", 10));
+
+        int age = 10;
+        int offset = 0;
+        int limit = 3;
+
+        // When
+        List<Member> members = memberJpaRepository.findByPage(age, offset, limit);
+        long totalCount = memberJpaRepository.totalCount(age);
+
+        // 페이지 계산 공식...
+        // toalPage = totalCount / size ...
+        // 마지막 페이지 ...
+        // 최초 페이지...
+
+        // Then
+        assertThat(members.size()).isEqualTo(3);
+        assertThat(totalCount).isEqualTo(5);
+    }
+
 }
